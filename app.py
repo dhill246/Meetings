b2ed23d1800aa0ddf5accc11a765c980fb1dd237
dhill246 <danielthill23@gmail.com>
2024-05-22 21:11:39 -0700

    Modify to be like old

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
+from werkzeug.utils import secure_filename
+import boto3
 
 app = Flask(__name__)
 
+BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        filename = secure_filename(file.filename)
+        
+        filepath = os.path.join("uploads", filename)
+        file.save(filepath)
+        
+        s3 = boto3.client("s3", region_name="us-east-1")
+
+        with open(filepath, "rb") as f:
+            s3.upload_fileobj(f, BUCKET_NAME, "test_file.jpg")
+        
+        return 'File uploaded successfully'
+
 
 # Main code
 if __name__ == '__main__':
