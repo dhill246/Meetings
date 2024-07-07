c6c20383ccf4f6b7b447e05528918b0e4722686d
dhill246 <danielthill23@gmail.com>
2024-07-06 21:01:05 -0700

    Add main files back

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import boto3
 from io import BytesIO
 import logging
 from datetime import datetime
-from utils.s3Uploads import check_existing_s3_files, upload_to_s3
+# from utils.s3Uploads import check_existing_s3_files, upload_to_s3
 
 # Initialize Flask app
 app = Flask(__name__)
@@ -15,6 +15,34 @@ app.secret_key = os.environ.get("SECRET_KEY")
 socketio = SocketIO(app)
 # Set up basic logging output for the app
 logging.basicConfig(level=logging.INFO)
+
+bucket_name = os.getenv('BUCKETEER_BUCKET_NAME')
+
+# Connect to S3 bucketeer
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('BUCKETEER_AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.getenv('BUCKETEER_AWS_SECRET_ACCESS_KEY'),
+)
+
+def check_existing_s3_files():
+    response = s3_client.list_objects_v2(Bucket=bucket_name)
+    list_of_files = []
+
+    # Check if 'Contents' key is in the response (it won't be if the bucket is empty)
+    if 'Contents' in response:
+        for item in response['Contents']:
+            print(item['Key'], item['LastModified'], item['Size'])
+            list_of_files.append(item["Key"])
+
+        return list_of_files
+    else:
+        print("No items in the bucket.")
+        return []
+    
+
+def upload_to_s3(audio_stream, key, bucket_name=bucket_name):
+    s3_client.upload_fileobj(audio_stream, bucket_name, key)
     
 @app.route('/', methods=["GET", "POST"])
 def index():
